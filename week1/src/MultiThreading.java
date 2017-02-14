a6331a622454864e91551de60974696665a9f34c
Peter Schriever <peterzen72@gmail.com>
2017-02-14 08:40:10 +0100

    restructured classes

--- week1/src/MultiThreading.java
+++ week1/src/MultiThreading.java
@@ -1,7 +1,4 @@
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.TextArea;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -19,74 +16,5 @@ public class MultiThreading extends Application {
     public void start(Stage stage) throws Exception {
         // 30.1
         TaskThreadDemo.main(null, stage);
-    }
-}
-
-
-// Week 1 code
-class TaskThreadDemo {
-    public static TextArea textArea = new TextArea();
-
-    public static void main(String[] args, Stage stage) {
-        // create the GUI
-        stage.setTitle("My first JavaFX application!");
-        VBox vbox = new VBox(TaskThreadDemo.textArea);
-        Scene scene = new Scene(vbox, 200, 100);
-        stage.setScene(scene);
-        stage.show();
-
-        // Create tasks
-        Runnable printA = new PrintChar('a', 100);
-        Runnable printB = new PrintChar('b', 100);
-        Runnable print100 = new PrintNum(100);
-
-        // Create threads
-        Thread thread1 = new Thread(printA);
-        Thread thread2 = new Thread(printB);
-        Thread thread3 = new Thread(print100);
-
-        // Start threads
-        thread1.start();
-        thread2.start();
-        thread3.start();
-    }
-}
-
-// The task for printing a character a specified number of times
-class PrintChar implements Runnable {
-    private char charToPrint; // The character to print
-    private int times; // The number of times to repeat
-
-    /** Construct a task with a specified character and number of
-     * times to print the character
-     */
-    public PrintChar(char c, int t) {
-        charToPrint = c;
-        times = t;
-    }
-    @Override /** Override the run() method to tell the system
-     * what task to perform
-     */
-    public void run() {
-        for (int i = 0; i < times; i++) {
-            TaskThreadDemo.textArea.appendText(""+charToPrint);
-        }
-    }
-}
-
-// The task class for printing numbers from 1 to n for a given n
-class PrintNum implements Runnable {
-    private int lastNum;
-
-    /** Construct a task for printing 1, 2, ..., n */
-    public PrintNum(int n) {
-        lastNum = n;
-    }
-
-    @Override /** Tell the thread how to run */
-    public void run() {
-        for (int i = 1; i <= lastNum; i++) {
-            TaskThreadDemo.textArea.appendText(" " + i);
-        }
     }
 }
