ebf194035ab326c336a11e3dd4b46e12dfa366e8
Peter Schriever <peterzen72@gmail.com>
2017-02-15 22:53:27 +0100

    cleaned up some code

---
+++ week1/src/JavaFX/RandomTicTacToe.java
@@ -0,0 +1,81 @@
+package JavaFX;
+
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.util.stream.IntStream;
+
+/**
+ * Created by peterzen on 2017-02-15.
+ * Part of the OOP3 project.
+ */
+class RandomTicTacToe {
+
+    RandomTicTacToe(Stage stage) {
+        stage.setTitle("Hello TicTacToe world!");
+
+        // prep TicTacToe grid
+        Group ticTacToeGrid = new Group();
+        Scene scene = new Scene(ticTacToeGrid);
+
+        // prepare and populate a 3*3 GridPane layout
+        GridPane layout = new GridPane();
+        layout.setGridLinesVisible(true); // NOTICE: for debugging only!
+        layout.setStyle("-fx-background-color: #d3d3d3; -fx-border-width: 5px; -fx-border-color: #222;");
+        IntStream.range(0, 3).forEach(column ->
+                IntStream.range(0, 3).forEach(row -> {
+                    TicTacToeCell cell = new TicTacToeCell();
+
+                    if (cell.iv.getImage() == null) {
+                        Pane emptyCell = new Pane();
+                        emptyCell.setMinHeight(50);
+                        emptyCell.setMinWidth(50);
+
+                        layout.add(emptyCell, column, row);
+                    } else {
+                        layout.add(cell.iv, column, row);
+                    }
+                })
+        );
+
+        ticTacToeGrid.getChildren().add(layout);
+
+        stage.setWidth(400);
+        stage.setHeight(300);
+        stage.setScene(scene);
+        stage.sizeToScene();
+        stage.show();
+    }
+
+    private class TicTacToeCell extends Pane {
+
+        ImageView iv;
+
+        TicTacToeCell() {
+            Image image = this.getCellImage(Math.random());
+            iv = new ImageView();
+
+            iv.setImage(image);
+            iv.setFitWidth(50);
+            iv.setPreserveRatio(true);
+            iv.setSmooth(true);
+            iv.setCache(true);
+        }
+
+        private Image getCellImage(double random) {
+            if (random > 0.66) {
+                return null;
+            } else if (random > 0.33) {
+                return new Image("o.gif");
+            } else {
+                return new Image("x.gif");
+            }
+        }
+    }
+
+}
